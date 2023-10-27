2c013ba89c26552aac46e06c9c3847bbeecd6124
devarjankars <devarjankars@gmail.com>
2023-10-27 11:13:51 +0530

    optimise chats

--- Contollers/homeControllers.js
+++ Contollers/homeControllers.js
@@ -1,36 +1,42 @@
 const Chat= require ('../Models/chat');
 const User= require('../Models/user');
 const path= require('path')
+const sequelize=require('../utils/database')
+const { Op } = require("sequelize");
+
+
 
 
 exports.sentMsg=async(req,res,next)=>{
-    try{
+    try {
+        await Chat.create({
+          name: req.user.name,
+          msg: req.body.message,
+          userId: req.user.id,
+        });
         
-   const result=await Chat.create({
-     name:req.user.name,
-     msg:req.body.msg,
-     userId:req.user.id
-   })
-   res.status(200).json({msg:"success"});
-
-    }
-    catch(err){
-        console.log(err);
-        res.status(400).json({msg:"failed"});
-    }
+        return res.status(200).json({ message: "Success!" });
+      } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error" });
+      }
 };
 
 exports.getMsg=async(req,res,next)=>{
-    try{
-        
-   const result= await Chat.findAll();
-   res.status(200).json({msg:result});
-
-    }
-    catch(err){
-        console.log(err);
-        res.status(400).json({msg:"failed"});
-    }
+    try {
+        const param = req.params.param;
+        const messages = await Chat.findAll({
+          where: {
+            id: {
+              [Op.gt]: param,
+            },
+          },
+        });
+        console.log(messages);
+        return res.status(200).json({ messages: messages });
+      } catch (error) {
+        console.log(error);
+      }
 };
 
 exports.getHomePage= async(req,res,next)=>{
--- Public/Js/chat.js
+++ Public/Js/chat.js
@@ -1,65 +1,339 @@
 
-const chatBtn=document.getElementById('send');
-const chatArea=document.getElementById('chat-area')
+// const chatBtn=document.getElementById('send');
+// const chatArea=document.getElementById('chat-area')
 
-let postChat=async()=>{
-    try{
-        const chat =document.getElementById('chat-text').value;
-        const token=localStorage.getItem('token')
-        let res=await axios.post('http://localhost:3000/home/sendMsg',{
-            msg:chat }
-           , { headers: { Authorization: token }})
-           console.log(res);
-           location.reload();
-        
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-
-
-
-
-let homepageLoad =async()=>{
-    try{
-        
-        const token=localStorage.getItem('token');
-      let result= await  axios.get(`http://localhost:3000/home/getMsg`,{
-            headers:{Authorization:token}
-        });
-        result.data.msg.forEach(ele => {
-         chatArea.innerHTML += `  <div class="d-flex flex-row justify-content-end mb-4">
-                    <div>
-                      <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">${ele.name}:${ele.msg}</p>
-                      </div>
-                      </div>`
-            
-        });
-        console.log(result);
-
-
-    }
-    catch(err){
-        console.log(err);
-    }
-
-}
-let reloadHomePage=async(req,res,next)=>{
-    location.reload();
-}
-
-
-
-document.addEventListener('DOMContentLoaded', homepageLoad)
-
-chatBtn.addEventListener('click',postChat)
-setInterval(reloadHomePage,10000)
-// let getMsg= async()=>{
+// let postChat=async()=>{
 //     try{
-
+//         const chat =document.getElementById('chat-text').value;
+//         const token=localStorage.getItem('token')
+//         let res=await axios.post('http://localhost:3000/home/sendMsg',{
+//             msg:chat }
+//            , { headers: { Authorization: token }})
+//            console.log(res);
+//            location.reload();
+        
 //     }
 //     catch(err){
 //         console.log(err);
 //     }
 // }
+
+
+
+
+// let homepageLoad =async()=>{
+//     try{
+        
+//         const token=localStorage.getItem('token');
+//       let result= await  axios.get(`http://localhost:3000/home/getMsg`,{
+//             headers:{Authorization:token}
+//         });
+//         result.data.msg.forEach(ele => {
+//          chatArea.innerHTML += `  <div class="d-flex flex-row justify-content-end mb-4">
+//                     <div>
+//                       <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">${ele.name}:${ele.msg}</p>
+//                       </div>
+//                       </div>`
+            
+//         });
+//         console.log(result);
+
+
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+
+// }
+// let reloadHomePage=async(req,res,next)=>{
+//     location.reload();
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded', homepageLoad)
+// // document.addEventListener("DOMContentLoaded", getMessagesFromLocalStorage);
+
+// chatBtn.addEventListener('click',postChat)
+// setInterval(reloadHomePage,10000)
+// // let getMsg= async()=>{
+// //     try{
+
+// //     }
+// //     catch(err){
+// //         console.log(err);
+// //     }
+// // }
+const messageTextArea = document.getElementById("messageTextArea");
+const messageSendBtn = document.getElementById("messageSendBtn");
+const chatBoxBody = document.getElementById("chatBoxBody");
+
+async function messageSend() {
+  try {
+    const message = messageTextArea.value;
+    const token = localStorage.getItem("token");
+    console.log(message);
+    const res = await axios.post(
+      "http://localhost:3000/home/sendMsg",
+      {
+        message: message,
+      },
+      { headers: { Authorization: token } }
+    );
+    console.log(res);
+    messageTextArea.value = "";
+  } catch (error) {
+    console.log("something went wrong");
+  }
+}
+
+function decodeToken(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+// const socket = io("http://localhost:5000");
+// socket.on("data", (data) => {
+//   console.log(data);
+// });
+
+// socket.emit("getMessages");
+
+// socket.on("messages", (messages) => {
+//   console.log(messages);
+//   const token = localStorage.getItem("token");
+//   const decodedToken = decodeToken(token);
+//   const userId = decodedToken.userId;
+//   chatBoxBody.innerHTML = "";
+
+//   messages.forEach((message) => {
+//     if (message.userId == userId) {
+//       const div = document.createElement("div");
+//       chatBoxBody.appendChild(div);
+
+//       const messageSendby = document.createElement("span");
+//       messageSendby.classList.add(
+//         "d-flex",
+//         "justify-content-end",
+//         "px-3",
+//         "mb-1",
+//         "text-uppercase",
+//         "small",
+//         "text-white"
+//       );
+//       messageSendby.appendChild(document.createTextNode("You"));
+//       div.appendChild(messageSendby);
+
+//       const messageBox = document.createElement("div");
+//       const messageText = document.createElement("div");
+
+//       messageBox.classList.add("d-flex", "justify-content-end", "mb-4");
+
+//       messageText.classList.add("msg_cotainer_send");
+//       messageText.appendChild(document.createTextNode(message.message));
+
+//       messageBox.appendChild(messageText);
+//       div.appendChild(messageBox);
+//     } else {
+//       const div = document.createElement("div");
+//       chatBoxBody.appendChild(div);
+
+//       const messageSendby = document.createElement("span");
+//       messageSendby.classList.add(
+//         "d-flex",
+//         "justify-content-start",
+//         "px-3",
+//         "mb-1",
+//         "text-uppercase",
+//         "small",
+//         "text-white"
+//       );
+//       messageSendby.appendChild(document.createTextNode(message.name));
+//       div.appendChild(messageSendby);
+
+//       const messageBox = document.createElement("div");
+//       const messageText = document.createElement("div");
+
+//       messageBox.classList.add("d-flex", "justify-content-start", "mb-4");
+
+//       messageText.classList.add("msg_cotainer");
+//       messageText.appendChild(document.createTextNode(message.message));
+
+//       messageBox.appendChild(messageText);
+//       div.appendChild(messageBox);
+//     }
+//   });
+// });
+
+async function getMessages() {
+  try {
+    let param;
+    const localStorageChats = JSON.parse(localStorage.getItem("chats"));
+    if (localStorageChats) {
+      let array = JSON.parse(localStorage.getItem("chats"));
+      let length = JSON.parse(localStorage.getItem("chats")).length;
+      param = array[length - 1].id;
+    }
+    const res = await axios.get(
+      `http://localhost:3000/home/getMsg/${param}`
+    );
+    const token = localStorage.getItem("token");
+    const decodedToken = decodeToken(token);
+    const userId = decodedToken.userId;
+    // chatBoxBody.innerHTML = "";
+    const chats = JSON.parse(localStorage.getItem("chats"));
+    if (!chats) {
+      localStorage.setItem("chats", JSON.stringify(res.data.messages));
+    } else {
+      res.data.messages.forEach((message) => {
+        chats.push(message);
+      });
+      localStorage.setItem("chats", JSON.stringify(chats));
+    }
+    res.data.messages.forEach((message) => {
+      if (message.userId == userId) {
+        const div = document.createElement("div");
+        chatBoxBody.appendChild(div);
+
+        const messageSendby = document.createElement("span");
+        messageSendby.classList.add(
+          "d-flex",
+          "justify-content-end",
+          "px-3",
+          "mb-1",
+          "text-uppercase",
+          "small",
+          "text-white"
+        );
+        messageSendby.appendChild(document.createTextNode("You"));
+        div.appendChild(messageSendby);
+
+        const messageBox = document.createElement("div");
+        const messageText = document.createElement("div");
+
+        messageBox.classList.add("d-flex", "justify-content-end", "mb-4");
+
+        messageText.classList.add("msg_cotainer_send");
+        messageText.appendChild(document.createTextNode(message.msg));
+
+        messageBox.appendChild(messageText);
+        div.appendChild(messageBox);
+      } else {
+        const div = document.createElement("div");
+        chatBoxBody.appendChild(div);
+
+        const messageSendby = document.createElement("span");
+        messageSendby.classList.add(
+          "d-flex",
+          "justify-content-start",
+          "px-3",
+          "mb-1",
+          "text-uppercase",
+          "small",
+          "text-white"
+        );
+        messageSendby.appendChild(document.createTextNode(message.name));
+        div.appendChild(messageSendby);
+
+        const messageBox = document.createElement("div");
+        const messageText = document.createElement("div");
+
+        messageBox.classList.add("d-flex", "justify-content-start", "mb-4");
+
+        messageText.classList.add("msg_cotainer");
+        messageText.appendChild(document.createTextNode(message.msg));
+
+        messageBox.appendChild(messageText);
+        div.appendChild(messageBox);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+setInterval(() => {
+  getMessages();
+}, 5000);
+
+async function getMessagesFromLocalStorage() {
+  const messages = JSON.parse(localStorage.getItem("chats"));
+
+  const token = localStorage.getItem("token");
+  const decodedToken = decodeToken(token);
+  const userId = decodedToken.userId;
+  chatBoxBody.innerHTML = "";
+
+  if (messages) {
+    messages.forEach((message) => {
+      if (message.userId == userId) {
+        const div = document.createElement("div");
+        chatBoxBody.appendChild(div);
+
+        const messageSendby = document.createElement("span");
+        messageSendby.classList.add(
+          "d-flex",
+          "justify-content-end",
+          "px-3",
+          "mb-1",
+          "text-uppercase",
+          "small",
+          "text-white"
+        );
+        messageSendby.appendChild(document.createTextNode("You"));
+        div.appendChild(messageSendby);
+
+        const messageBox = document.createElement("div");
+        const messageText = document.createElement("div");
+
+        messageBox.classList.add("d-flex", "justify-content-end", "mb-4");
+
+        messageText.classList.add("msg_cotainer_send");
+        messageText.appendChild(document.createTextNode(message.msg));
+
+        messageBox.appendChild(messageText);
+        div.appendChild(messageBox);
+      } else {
+        const div = document.createElement("div");
+        chatBoxBody.appendChild(div);
+
+        const messageSendby = document.createElement("span");
+        messageSendby.classList.add(
+          "d-flex",
+          "justify-content-start",
+          "px-3",
+          "mb-1",
+          "text-uppercase",
+          "small",
+          "text-white"
+        );
+        messageSendby.appendChild(document.createTextNode(message.name));
+        div.appendChild(messageSendby);
+
+        const messageBox = document.createElement("div");
+        const messageText = document.createElement("div");
+
+        messageBox.classList.add("d-flex", "justify-content-start", "mb-4");
+
+        messageText.classList.add("msg_cotainer");
+        messageText.appendChild(document.createTextNode(message.msg));
+
+        messageBox.appendChild(messageText);
+        div.appendChild(messageBox);
+      }
+    });
+  }
+}
+
+messageSendBtn.addEventListener("click", messageSend);
+document.addEventListener("DOMContentLoaded", getMessagesFromLocalStorage);
