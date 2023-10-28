d32787ff1031b1f6198f833d5f4e426866ddb484
devarjankars <devarjankars@gmail.com>
2023-10-28 20:12:55 +0530

    add group admin

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //db import
+const Group= require('./Models/group');
+const groupRouter= require('./routes/gruopRoute');
 const sequelize= require('./utils/database');
 const User= require('./Models/user');
 const Chat = require('./Models/chat')
@@ -18,20 +20,34 @@ const Chat = require('./Models/chat')
 //routes import
 const userRoutes= require('./routes/userRoute');
 const homeRoutes=require('./routes/homeRoute');
+const UserGroup = require('./Models/userGroup');
 
 
 //rotes redirection
 app.use('/',userRoutes);
 app.use('/home',homeRoutes);
+app.use("/group", groupRouter);
 
 //db assoction
-User.hasMany(Chat);
+User.hasMany(Chat,{ onDelete: "CASCADE", hooks: true });
 Chat.belongsTo(User)
+//user to group
+User.hasMany(Group)
+
+//group to chat
+Group.hasMany(Chat);
+Chat.belongsTo(Group)
+//UserGroup at juction table 
+UserGroup.belongsTo(User);
+UserGroup.belongsTo(Group);
+Group.hasMany(UserGroup)
 
 //db initalization
 
 sequelize
-  .sync()
+  .sync(
+//{ force:true}
+  )
   .then((result) => {
     app.listen(process.env.PORT || 3000);
   })
---
+++ routes/gruopRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const groupController = require("../Contollers/groupController");
+const Authentication = require("../middleware/authy");
+
+router.post("/createGroup", Authentication, groupController.createGroup);
+
+router.post("/addToGroup", Authentication, groupController.addToGroup);
+
+router.get("/getGroups", Authentication, groupController.getGroups);
+
+router.post(
+  "/deleteFromGroup",
+  Authentication,
+  groupController.deleteFromGroup
+);
+
+router.get(
+  "/groupMembers/:groupName",
+  Authentication,
+  groupController.groupMembers
+);
+
+module.exports = router;
